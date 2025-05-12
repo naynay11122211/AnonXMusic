d9b8c27fe4b9f83396d1e957a7e00efa1192bcc3
SANATANI TECH <sachinxopx007@gmail.com>
2025-05-12 12:07:37 +0530

    .
    Signed-off-by: SANATANI TECH <sachinxopx007@gmail.com>

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -9,10 +9,9 @@ from pyrogram.enums import MessageEntityType
 from concurrent.futures import ThreadPoolExecutor
 from youtubesearchpython.__future__ import VideosSearch, CustomSearch
 
-from AnonXmusic import LOGGER
-from AnonXmusic.utils.database import is_on_off
-from AnonXmusic.utils.formatters import time_to_seconds
-
+from AnonXMusic import LOGGER
+from AnonXMusic.utils.database import is_on_off
+from AnonXMusic.utils.formatters import time_to_seconds
 
 #=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×[ NO NEED COOKIES ]=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×
 
